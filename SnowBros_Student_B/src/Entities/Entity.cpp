c11a6a80fb045025ce3a57252d495d30065bd619
Arslan <i253101@isb.nu.edu.pk>
2026-05-04 00:22:45 +0500

    Revert "modular now"
    This reverts commit 1df21ff2a22a170c73148e6020fd20d0f7dd2534.

--- SnowBros_Student_B/src/Entities/Entity.cpp
+++ SnowBros_Student_B/src/Entities/Entity.cpp
@@ -1,2 +1,34 @@
 #include "Entity.h"
-// Implementation moved inline to Entity.h during OOP refactoring.
+
+Entity::Entity(const sf::Vector2f& position)
+    : position(position), velocity(0.f, 0.f), active(true)
+{
+    hitbox = sf::FloatRect(position, sf::Vector2f(0.f, 0.f));
+}
+
+sf::Vector2f Entity::GetPosition() const
+{
+    return position;
+}
+
+void Entity::SetPosition(const sf::Vector2f& pos)
+{
+    position = pos;
+    hitbox.position.x = position.x;
+    hitbox.position.y = position.y;
+}
+
+sf::FloatRect Entity::GetHitBox() const
+{
+    return hitbox;
+}
+
+bool Entity::IsActive() const
+{
+    return active;
+}
+
+void Entity::Deactivate()
+{
+    active = false;
+}
